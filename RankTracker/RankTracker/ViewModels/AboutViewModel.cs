3bd1acffee4e471ecadbe9ee4583fda7e83345c2
Szymon Kujawski <KujawskiSzymon0@gmail.com>
2021-07-09 08:24:21 +0200

    Credits page

--- RankTracker/RankTracker/ViewModels/AboutViewModel.cs
+++ RankTracker/RankTracker/ViewModels/AboutViewModel.cs
@@ -9,8 +9,8 @@ namespace RankTracker.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            Title = "Credits";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/KujawskiSzymon/RankTracker"));
         }
 
         public ICommand OpenWebCommand { get; }
